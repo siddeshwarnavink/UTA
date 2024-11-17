5f9a4f67720e7ef6461583bc65871ce7595edd25
Barath <kvbarath2004@gmail.com>
2024-11-17 20:27:17 +0530

    adapter message changed

--- shared/utils/adapter_message.go
+++ shared/utils/adapter_message.go
@@ -2,9 +2,15 @@ package utils
 
 import "fmt"
 
+//AdapterHeader is the header of the adapter message
+//First 4 bytes of the message are the version
+//Next bit is if the message is for adapter or not(1 for adapter, 0 for not)
+//Next 3 bits are reserved for future use
+var AdapterHeader = byte{0x18}
+
 func GetAdapterMessage(buf []byte) (string, error) {
 	header := buf[0]
-	if header == 0x18 {
+	if header == AdapterHeader {
 		return string(buf[1:]), nil
 	}
 	return "", fmt.Errorf("invalid adapter message header: %x", header)
@@ -12,7 +18,7 @@ func GetAdapterMessage(buf []byte) (string, error) {
 
 func GenerateAdapterMessage(message string) []byte {
 	buf := make([]byte, len(message)+1)
-	buf[0] = 0x18
+	buf[0] = AdapterHeader
 	copy(buf[1:], message)
 	return buf
 }

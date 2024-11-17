200d1471c48682cfb7b197c6c5ddd88e198d13c4
Barath <kvbarath2004@gmail.com>
2024-11-17 20:15:50 +0530

    msg format and readme updated

--- shared/utils/adapter_message.go
+++ shared/utils/adapter_message.go
@@ -1,51 +1,18 @@
 package utils
 
-import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"time"
-)
-
-type AdapterMessage struct {
-	To        string `json:"to"`
-	Payload   string `json:"payload"`
-	Timestamp int64  `json:"timestamp"`
-}
+import "fmt"
 
 func GetAdapterMessage(buf []byte) (string, error) {
-
-	var obj map[string]interface{}
-	err := json.Unmarshal(buf, &obj)
-
-	if err != nil {
-		return "not json", err
+	header := buf[0]
+	if header == 0x18 {
+		return string(buf[1:]), nil
 	}
-
-	val, ok := obj["to"]
-	if !ok || val != "adapter" {
-		return "", errors.New("not to adapter")
-	}
-
-	payload, ok := obj["payload"].(string)
-	if !ok {
-		return "", errors.New("no payload")
-	}
-	return payload, nil
+	return "", fmt.Errorf("invalid adapter message header: %x", header)
 }
 
-func GenerateAdapterMessage(payload string) []byte {
-	unixTimestamp := time.Now().Unix()
-	message := AdapterMessage{
-		To:        "adapter",
-		Timestamp: unixTimestamp,
-		Payload:   payload,
-	}
-
-	responseJson, err := json.Marshal(message)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-
-	return responseJson
+func GenerateAdapterMessage(message string) []byte {
+	buf := make([]byte, len(message)+1)
+	buf[0] = 0x18
+	copy(buf[1:], message)
+	return buf
 }

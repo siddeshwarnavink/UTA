a061f5e3a935b15590ce4c9b3bfb765f8b6c99b4
Siddeshwar <siddeshwar.work@gmail.com>
2024-12-04 04:47:13 +0530

    liveadapters useless stylings

--- wizard/src/components/pages/LiveAdapters.jsx
+++ wizard/src/components/pages/LiveAdapters.jsx
@@ -1,14 +1,86 @@
-import { faComputer, faServer, faTable, faUpload, faDownload } from '@fortawesome/free-solid-svg-icons';
+import { useMemo } from 'react';
+import { faComputer, faServer, faUpload, faDownload, faHatWizard } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Container, Table } from 'react-bootstrap';
-
+import { Col, Container, Row, Table, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+
+import classes from './LiveAdapters.module.css';
+import liveIcon from '../../assets/live.gif';
 
 const LiveAdapters = ({ routingTable, transmission }) => {
+
+  const noClients = useMemo(() => {
+    if (Object.keys(routingTable).length > 0) {
+      return Object.entries(routingTable)
+        .filter(([_, item]) => item.role === "adapter-client")
+        .length;
+    }
+    return 0;
+  }, [routingTable]);
+
+  const noServer = useMemo(() => {
+    if (Object.keys(routingTable).length > 0) {
+      return Object.entries(routingTable)
+        .filter(([_, item]) => item.role === "adapter-server")
+        .length;
+    }
+    return 0;
+  }, [routingTable]);
+
+  const noWizard = useMemo(() => {
+    if (Object.keys(routingTable).length > 0) {
+      return Object.entries(routingTable)
+        .filter(([_, item]) => item.role === "wizard")
+        .length;
+    }
+    return 0;
+  }, [routingTable]);
+
   return (
     <Container>
+      <Row className={classes.useless_thing}>
+        <Col>
+          <Card className={[classes.card, classes.cyan].join(" ")}>
+            <div>
+              <div className={classes.count}>{noClients}</div>
+              <div classname={classes.label}>
+                Client
+              </div>
+            </div>
+            <div className={classes.icon}>
+              <FontAwesomeIcon icon={faComputer} />
+            </div>
+          </Card>
+        </Col>
+        <Col>
+          <Card className={[classes.card, classes.yellow].join(" ")}>
+            <div>
+              <div className={classes.count}>{noServer}</div>
+              <div classname={classes.label}>Server</div>
+            </div>
+            <div className={classes.icon}>
+              <FontAwesomeIcon icon={faServer} />
+            </div>
+          </Card>
+        </Col>
+        <Col>
+          <Card className={[classes.card, classes.red].join(" ")}>
+            <div>
+              <div className={classes.count}>{noWizard}</div>
+              <div classname={classes.label}>
+                Wizard
+              </div>
+            </div>
+            <div className={classes.icon}>
+              <FontAwesomeIcon icon={faHatWizard} />
+            </div>
+          </Card>
+        </Col>
+      </Row>
       <h3 className="my-4">
-        <FontAwesomeIcon icon={faTable} />{" "}Live Adapters
+        <img src={liveIcon} alt="Live now..." className={classes.liveIcon} />
+        Live Adapters
       </h3>
       <Table>
         <thead>
@@ -43,18 +115,20 @@ const LiveAdapters = ({ routingTable, transmission }) => {
                   </td>
                   <td>{peer.role === "adapter-client" ? (
                     <span>
-                      <FontAwesomeIcon icon={faComputer} />{" "}
+                      <FontAwesomeIcon icon={faComputer} style={{ color: "#1c702f" }} />{" "}
                       Client
                     </span>
                   ) : peer.role === "adapter-server" ? (
                     <span>
-                      <FontAwesomeIcon icon={faServer} />{" "}
+                      <FontAwesomeIcon icon={faServer} style={{ color: "#cb9906" }} />{" "}
                       Server
                     </span>
                   ) : peer.role}</td>
                   <td>{peer.from_ip}</td>
                   <td>{peer.to_ip}</td>
-                  <td>{new Date(peer.last_seen).toLocaleString()}</td>
+                  <td>
+                    {moment(peer.last_seen).fromNow()}
+                  </td>
                 </tr>
               ))
           ) : (
